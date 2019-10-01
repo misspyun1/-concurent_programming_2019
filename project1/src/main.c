278294212cd0b1d439b2b0e16318b16039b8f627
편규리 <2016026053@hanyang.ac.kr>
2019-10-01 16:01:18 +0900

    less num of read write

--- project1/src/main.c
+++ project1/src/main.c
@@ -67,7 +67,7 @@ void split ( int input_fd )
 
 	size_t file_size = lseek(input_fd, 0, SEEK_END);
 	size_t file_size_half[2];
-	file_size_half[0]= (file_size /TUPLE_SIZE)/2 * TUPLE_SIZE;
+	file_size_half[0]= file_size/2;
 	file_size_half[1] = file_size - file_size_half[0];
 
 	size_t  offset = 0;
@@ -107,101 +107,122 @@ void sort ( int output_fd )
     int temp_fd[2] ; // file descriptor of temp files
     size_t temp_file_offset[2] = {0,0};
     size_t output_offset = 0;
-    char *temp_file_names[ ] =
-            {
-                    "temp1.dat",
-                    "temp2.dat",
-            } ;
+    char *temp_file_names[ ] ={"temp1.dat","temp2.dat",} ;
+    size_t temp_file_size[2];
+    size_t temp_file_read_amount =  ((temp_file_size[0]/TUPLE_SIZE)/4 +1)*TUPLE_SIZE;
 
     size_t ret;
 
     char *buffer1;
-	buffer1 = (char *) malloc(sizeof(char) * 100);    
+	buffer1 = (char *) malloc(sizeof(char) * MEM_SIZE/4);    
     char *buffer2;
-	buffer2 = (char *) malloc(sizeof(char) * 100);
+	buffer2 = (char *) malloc(sizeof(char) * MEM_SIZE/4);
+    char *output_buffer;
+	output_buffer = (char *) malloc(sizeof(char) * MEM_SIZE/2);
 
     int  flag1, flag2;
+    int buffer1_idx=0, buffer2_idx=0;
+    size_t read_ret1, read_ret2;
 
-    for (int i = 0 ; i <= 1 ; i++ ){
+    for (int i = 0 ; i <2 ; i++ ){
         temp_fd[i] = open(temp_file_names[i], O_RDWR, 0777);
         if (temp_fd[i] == -1) {
             printf("error: open temp file\n");
             return;
         }
 
-        size_t file_size = lseek(temp_fd[i], 0, SEEK_END);
-        if ( file_size == 0 ){
+        temp_file_size[i] = lseek(temp_fd[i], 0, SEEK_END);
+        if ( temp_file_size[i] == 0 ){
             printf("error : temp file empty\n");
             return;
         }
     }
 
-    flag1 = flag2 = 1 ;
+    flag1 = flag2 = 0 ;
+    read_ret1 = pread(temp_fd[0], buffer1,temp_file_read_amount, temp_file_offset[0]);
+    read_ret2 = pread(temp_fd[1], buffer2, temp_file_read_amount, temp_file_offset[1]);
+    temp_file_offset[0] += read_ret1;
+    temp_file_offset[1] += read_ret2;
 
     while ( 1 )
     {
         if ( flag1 )
         {
-            size_t read_ret = pread(temp_fd[0], buffer1, TUPLE_SIZE, temp_file_offset[0]);
-            if ( read_ret == 0 )
+            read_ret1 = pread(temp_fd[0], buffer1, temp_file_read_amount, temp_file_offset[0]);
+            if ( read_ret1 == 0 )
             {   /* If first file ends then the whole content of second
                     file is written in the respective target file */
-                ret = pwrite(output_fd, buffer2, TUPLE_SIZE, output_offset);
+                ret = pwrite(output_fd, buffer2+buffer2_idx, read_ret2-buffer2_idx, output_offset);
                 output_offset += ret;
                 while (1){
-                    read_ret = pread ( temp_fd[1], buffer2, TUPLE_SIZE, temp_file_offset[1] );
-                    if(read_ret == 0){
+                    read_ret2 = pread ( temp_fd[1], output_buffer, MEM_SIZE/2, temp_file_offset[1] );
+                    //printf("temp file2가 모자라서 %u만큼 더 읽어요\n", (unsigned int)read_ret2);
+                    if(read_ret2 == 0){
                         break;
                     }
-                    ret = pwrite(output_fd, buffer2, TUPLE_SIZE, output_offset);
+                    ret = pwrite(output_fd, output_buffer, read_ret2, output_offset);
                     output_offset += ret;
-                    temp_file_offset[1] += read_ret;
+                    temp_file_offset[1] += read_ret2;
                 }
+                
                 break ;
             }
-            temp_file_offset[0] += read_ret;
+            temp_file_offset[0] += read_ret1;
         }
 
         if ( flag2 )
         {
-            size_t read_ret = pread(temp_fd[1], buffer2, TUPLE_SIZE, temp_file_offset[1]);
-            if ( read_ret == 0 )
+            read_ret2 = pread(temp_fd[1], buffer2,temp_file_read_amount, temp_file_offset[1]);
+            if ( read_ret2 == 0 )
             {   /* If first file ends then the whole content of second
                     file is written in the respective target file */
-                ret = pwrite(output_fd, buffer1, TUPLE_SIZE, output_offset);
+                ret = pwrite(output_fd, buffer1 + buffer1_idx, read_ret1-buffer1_idx, output_offset);
                 output_offset += ret;
                 while (1){
-                    read_ret = pread ( temp_fd[0], buffer1, TUPLE_SIZE, temp_file_offset[0] );
-                    if(read_ret == 0){
+                    read_ret1 = pread ( temp_fd[0], output_buffer, MEM_SIZE/2, temp_file_offset[0] );
+                    //printf("temp file1가 모자라서 %u만큼 더 읽어요\n", (unsigned int)read_ret1);
+                    //printf("%s",output_buffer);
+                    if(read_ret1 == 0){
                         break;
                     }
-                    ret = pwrite(output_fd, buffer1, TUPLE_SIZE, output_offset);
+                    ret = pwrite(output_fd, output_buffer, read_ret1, output_offset);
                     output_offset += ret;
-                    temp_file_offset[0] += read_ret;
+                    temp_file_offset[0] += read_ret1;
                 }
                 break ;
             }
-            temp_file_offset[1] += read_ret;
+            temp_file_offset[1] += read_ret2;
+        }
+        
+        buffer1_idx = 0;
+        buffer2_idx = 0;
+        size_t output_amount = 0;
+        while(buffer1_idx < read_ret1 && buffer2_idx < read_ret2){
+            int cmp = compare(buffer1+buffer1_idx, buffer2+buffer2_idx);
+            //printf("(%d, %d) / (%u, %u): \n",buffer1_idx, buffer2_idx,(unsigned int) read_ret1, (unsigned int)read_ret2);
+            //printf("%s%s -->%d\n", buffer1+buffer1_idx, buffer2+buffer2_idx, cmp);
+            if ( cmp < 0)
+            {
+                flag2 = 0 ;
+                flag1 = 1 ;
+                memcpy(output_buffer+output_amount, buffer1+buffer1_idx, TUPLE_SIZE);
+                buffer1_idx += TUPLE_SIZE;
+            }
+            else
+            {
+                flag1 = 0 ;
+                flag2 = 1 ;
+                memcpy(output_buffer+output_amount, buffer2+buffer2_idx, TUPLE_SIZE);
+                buffer2_idx += TUPLE_SIZE;
+            }
+            if (ret < 0) {
+                printf("error: write temp file\n");
+                return;
+            }
+            output_amount += TUPLE_SIZE;
         }
 
-        int cmp = compare(buffer1, buffer2);
-        //printf("%s%s -->%d\n", buffer1, buffer2, cmp);
-        if ( cmp < 0)
-        {
-            flag2 = 0 ;
-            flag1 = 1 ; 
-            ret = pwrite(output_fd, buffer1, TUPLE_SIZE, output_offset);
-        }
-        else
-        {
-            flag1 = 0 ;
-            flag2 = 1 ;
-            ret = pwrite(output_fd, buffer2, TUPLE_SIZE, output_offset);
-        }
-        if (ret < 0) {
-			printf("error: write temp file\n");
-			return;
-		}
+        ret = pwrite(output_fd, output_buffer, output_amount, output_offset);
         output_offset += ret;
 
     }
@@ -209,6 +230,7 @@ void sort ( int output_fd )
     // free buffer.
 	free(buffer1);
 	free(buffer2);
+    free(output_buffer);
 
 	close(temp_fd[0]);
 	close(temp_fd[1]);
